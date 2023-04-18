7eaff3b1d90f61fc5779c5e33fbecbf0351076c7
Igor <glock.shakur@yandex.ru>
2023-04-18 13:49:46 +0400

    work with i18, ru-eng translate

--- src/shared/ui/ThemeSwitcher/ui/ThemeSwitcher.tsx
+++ src/shared/ui/ThemeSwitcher/ui/ThemeSwitcher.tsx
@@ -17,7 +17,7 @@ export const ThemeSwitcher = ({className}: ThemeSwitcherProps) => {
   className={classNames(cls.ThemeSwitcher, {}, [className])} 
   onClick={toggleTheme}
   >
-    {theme === Theme.DARK ? <LightIcon /> : <DarkIcon />}
+    {theme === Theme.DARK ? <DarkIcon /> : <LightIcon />}
     </Button>
  );
 };
