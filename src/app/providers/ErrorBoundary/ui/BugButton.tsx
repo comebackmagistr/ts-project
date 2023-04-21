b13873731f933caeb8bbda88f16193504190ef39
Igor <glock.shakur@yandex.ru>
2023-04-21 12:16:11 +0400

    add error boundary

---
+++ src/app/providers/ErrorBoundary/ui/BugButton.tsx
@@ -0,0 +1,26 @@
+import { classNames } from 'shared/lib/classNames/classNames';
+import { useEffect, useState } from 'react';
+import { useTranslation } from 'react-i18next';
+
+
+// Компонент для тестирования ErrorBoundary
+export const BugButton = () => {
+
+    const [error, setError] = useState(false)
+    const {t} = useTranslation()
+    const onThrow = () => setError(true)
+
+    useEffect(() => {
+        if(error) {
+            throw new Error
+        }
+    }, [error])
+
+    return (
+        <button 
+            onClick={onThrow}
+        >
+            {t('throw error')}
+        </button>
+    );
+};
