80c523aa17d16d5563dd0c9a7efa38ed55855803
Igor <glock.shakur@yandex.ru>
2023-04-24 12:35:23 +0400

    add bundle analyzer

--- src/index.tsx
+++ src/index.tsx
@@ -6,6 +6,7 @@ import { ThemeProvider } from 'app/providers/ThemeProvider'
 import 'shared/config/i18n/i18n'
 import { ErrorBoundary } from 'app/providers/ErrorBoundary'
 
+
 render(
     <BrowserRouter>
         <ErrorBoundary>
